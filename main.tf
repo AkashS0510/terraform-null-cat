bcfe4c2787fe0f0d95e0f888fb05c8d385d47df8
Akash S <96624761+AkashS0510@users.noreply.github.com>
2025-10-28 17:15:05 +0530

    Add Terraform configuration for artifact creation
    This Terraform configuration defines a local provider, a variable for artifact content, and resources to create a file and execute a command. It also includes multiple output values.

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "local" {}
+
+variable "artifact_content" {
+  description = "Content to be written2 to the artifact file"
+  type        = string
+  default     = "This is an artifact22 created by Terraform"
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo '${var.artifact_content}' > artifact.txt"
+  }
+}
+
+resource "local_file" "artifact" {
+  content  = var.artifact_content
+  filename = "${path.module}/artifact.txt"
+}
+
+output "cat_ghost" {
+  value = "Ghost meawed successfully!"
+}
+
+
+output "cat_is_not_ghost" {
+  value = "Ghost meawed successfully!"
+}
+
+output "artifact_content" {
+  value = local_file.artifact.content
+}
