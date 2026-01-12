61bcca4ed95cfea50a7a1ce9d3e9c10e8d36fcea
Akash S <96624761+AkashS0510@users.noreply.github.com>
2026-01-12 22:49:38 +0530

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
 provider "local" {}
 
+variable "artifact_content" {
+  description = "Content to be written23 to the artifact file"
+  type        = string
+  default     = "This is an artifact22 created by Terraform"
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo '${var.artifact_content}' > artifact.txt"
+  }
+}
+
+resource "local_file" "artifact" {
+  content  = var.artifact_content
+  filename = "${path.module}/artifact.txt"
+}
 
 output "cat_ghost" {
   value = "Ghost meawed successfully!"
